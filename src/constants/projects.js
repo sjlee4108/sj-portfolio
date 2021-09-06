11a7925b761aebee32e2c51b925403ad5e80d750
Seungjae Lee <sjlee4108@gmail.com>
2021-09-05 21:49:51 -0400

    added new styles

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -9,23 +9,6 @@ export const projects = {
       term: 'Spring 2021 - Summer 2021',
       image: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg',
       description: `
-        Positive Tracks is a non-profit organization that helps youth make social changes through physical activity.
-        As a senior capstone project, our team (a group of 4 developers) created a event platform webpage where organizers can host/post an event, 
-        Positive Tracks admin can assist organizers, and participants can browse/join events.
-      `.replace(/\n/g, ' '),
-      bullets: [
-      'Designed webpage prototype (Figma). Went through 3 different iterations based on the feedbacks from Positive Tracks and past organizers',
-      'Created dashboards(organizer, admin), homepage, event signup page, and event info page and highly reusable components that go with it (buttons, tabs, textboxes ...)',
-      'Received positive feedbacks from Positive Tracks and past user on the beta version',
-      'Full version expected to be released from Positive Tracks'],
-    },
-    {
-      title: 'Positive Tracks: Event Platform for Youth Activism',
-      position: 'Designer & Front-end',
-      tags: ['React', 'Figma', 'Redux'],
-      term: 'Fall 2020 - Winter 2021',
-      image: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg',
-      description: `
         Vitalize is a startup that provides trauma-informed digital wellness platform tailored to healthcare providers. 
         As a full-stack intern, I worked on creating meditation and journal pages for the app (react-native) and created corresponding backend API calls (Node.js and MongoDB).
       `.replace(/\n/g, ' '),
@@ -36,6 +19,24 @@ export const projects = {
       'Beta version expected to be launched in September and tested by clinicians.'],
     },
     {
+      title: 'Positive Tracks: Event Platform for Youth Activism',
+      position: 'Designer & Front-end',
+      tags: ['React', 'Figma', 'Redux'],
+      term: 'Fall 2020 - Winter 2021',
+      image: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg',
+
+    description: `
+      Positive Tracks is a non-profit organization that helps youth make social changes through physical activity.
+      As a senior capstone project, our team (a group of 4 developers) created a event platform webpage where organizers can host/post an event, 
+      Positive Tracks admin can assist organizers, and participants can browse/join events.
+      `.replace(/\n/g, ' '),
+    bullets: [
+      'Designed webpage prototype (Figma). Went through 3 different iterations based on the feedbacks from Positive Tracks and past organizers',
+      'Created dashboards(organizer, admin), homepage, event signup page, and event info page and highly reusable components that go with it (buttons, tabs, textboxes ...)',
+      'Received positive feedbacks from Positive Tracks and past user on the beta version',
+      'Full version expected to be released from Positive Tracks'],
+    },
+    {
       title: 'Dynamic Navigation System for Indoor Robots',
       position: 'Research Assistant',
       term: 'Summer 2019 - Fall 2019',
@@ -44,7 +45,7 @@ export const projects = {
       description: 'hi2',
     },
     {
-      title: 'Modeling Spatial Disorientation in Circular Motions',
+      title: 'Spatial Disorientation in Circular Motions',
       position: 'Research Assistant',
       tags: ['Research', 'Mathematical Modeling', 'MATLAB'],
       image: 'https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg',
