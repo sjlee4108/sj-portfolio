a863711dcf6908feef65d23db2f8116700bfccfa
Seungjae Lee <sjlee4108@gmail.com>
2021-09-18 00:08:21 -0400

    small change

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Home from './themeChanger/homeScreen/homeScreen';
 import NavBar from './navBar/navBar';
 import Home from './homeScreen/homeScreen';
 
