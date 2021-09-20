3bac2aeddc5e52b50c037335ee4739766cbbad30
Seungjae Lee <sjlee4108@gmail.com>
2021-09-20 10:47:58 -0400

    remodeling projects

---
+++ src/components/projectSection/tag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './tagStyles.scss';
+
+const Tag = (props) => {
+  const { value } = props;
+  return (
+    <div className={styles.tagContainer}>
+      {value}
+    </div>
+  );
+};
+
+export default Tag;
